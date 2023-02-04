78c1db4af28ea15fa17acedca958db6ce816c734
LUkasPacon <lukas@pacon.cz>
2023-02-04 19:16:12 +0100

    New version

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 ECHO "Hello!<br>";
 echo "Welcome to Developer News<br>";
-EcHo "Enjoy all ogrgrgegegergf the ad-free articles<br>";
+EcHo "Enjoy all ogrgrgegegergf the ad-free articles2222222<br>";
 EcHo "Enjoy all of the ad-ree rtigrgcles<br>";
 
